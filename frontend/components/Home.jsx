3bba704528544e27e33e6b4ef002fff0ac22af71
Hersha Venkatesh <harshavenk2003@yahoo.com>
2020-03-13 12:29:29 -0700

    Image

--- frontend/components/Home.jsx
+++ frontend/components/Home.jsx
@@ -20,6 +20,7 @@ export default class Home extends Component {
 						/>
 
 				</video> */}
+				
 					<div className="leftPage">
 						<div className="benefitStatment">
 							Quack! replaces email inside your flock!
