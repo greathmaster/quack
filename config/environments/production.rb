b03c12a1fd29db15915491730f76c81d1e7606ed
Hersha Venkatesh <harshavenk2003@yahoo.com>
2020-03-30 21:11:37 -0700

    Prepare for deployment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,10 +1,6 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
-  config.web_socket_server_url = "wss://quackit.herokuapp.com/cable" 
-  config.action_cable.allowed_request_origins = ['https://quackit.herokuapp.com', 'http://quackit.herokuapp.com']
-
-
   # Code is not reloaded between requests.
   config.cache_classes = true
 
@@ -53,6 +49,9 @@ config.active_storage.service = :amazon_prod
   # config.action_cable.mount_path = nil
   # config.action_cable.url = 'wss://example.com/cable'
   # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
+  config.web_socket_server_url = "wss://quackit.herokuapp.com/cable" 
+  config.action_cable.allowed_request_origins = ['https://quackit.herokuapp.com', 'http://quackit.herokuapp.com']
+
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
